246c64cc2698ffce1932bca8627933e51bd9b7e9
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-11 21:16:57 +0800

    used stacking in a for loop in python interpreter

--- queues.py
+++ queues.py
@@ -46,3 +46,9 @@ class Queue:
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
+
+# from queues import Stack
+
+# lifo=Stack("1st", "2nd", "3rd")
+# for element in lifo:
+#     print(element)
