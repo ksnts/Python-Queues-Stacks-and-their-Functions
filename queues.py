b468f99dc8a0b9ec56f5c7d736845cc7705e7063
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-11 22:01:56 +0800

    Used python interpreter to use priority queues with the functions

--- queues.py
+++ queues.py
@@ -64,3 +64,17 @@ class PriorityQueue:
 
     def dequeue(self):
         return heappop(self._elements)
+
+# >>> from queues import PriorityQueue
+
+# >>> CRITICAL = 3
+# >>> IMPORTANT = 2
+# >>> NEUTRAL = 1
+
+# >>> messages = PriorityQueue()
+# >>> messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+# >>> messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+# >>> messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+# >>> messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+# >>> messages.dequeue()
