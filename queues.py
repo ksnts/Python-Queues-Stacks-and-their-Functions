d2a827735ebbfd55af3ad16ae5756d6c81a667a3
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-11 22:21:32 +0800

    altered the priority functions so that critical messages now come first

--- queues.py
+++ queues.py
@@ -60,21 +60,22 @@ class PriorityQueue:
         self._elements = []
 
     def enqueue_with_priority(self, priority, value):
-        heappush(self._elements, (priority, value))
+        heappush(self._elements, (-priority, value))
 
     def dequeue(self):
-        return heappop(self._elements)
+        return heappop(self._elements)[1]
 
-# >>> from queues import PriorityQueue
+# from queues import PriorityQueue
 
-# >>> CRITICAL = 3
-# >>> IMPORTANT = 2
-# >>> NEUTRAL = 1
+# CRITICAL = 3
+# IMPORTANT = 2
+# NEUTRAL = 1
 
-# >>> messages = PriorityQueue()
-# >>> messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
-# >>> messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
-# >>> messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
-# >>> messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+# messages = PriorityQueue()
+# messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+# messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+# messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+# messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
 
-# >>> messages.dequeue()
+# messages.dequeue()
+
