dd6247fb761c0a9f6f6f7d188dfddec1121905bc
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-12 23:26:19 +0800

    First draft for thread safe queues in python

---
+++ thread_safe_queues.py
@@ -0,0 +1,11 @@
+import argparse
+from queue import LifoQueue, PriorityQueue, Queue
+
+QUEUE_TYPES = {
+    "fifo": Queue,
+    "lifo": LifoQueue,
+    "heap": PriorityQueue
+}
+
+def main(args):
+    buffer = QUEUE_TYPES[args.queue]()
