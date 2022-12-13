f6fb1705800fe5b021f7c10a913f4b9e3c13dca1
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-13 23:07:35 +0800

    added class producer

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -3,6 +3,7 @@ from queue import LifoQueue, PriorityQueue, Queue
 import threading
 from random import randint
 from time import sleep
+from random import choice, randint
 
 QUEUE_TYPES = {
     "fifo": Queue,
@@ -74,6 +75,18 @@ class Worker(threading.Thread):
         for _ in range (100):
             sleep(delay / 100)
             self.progress += 1
+
+class Producer(Worker):
+    def __init__(self, speed, buffer, products):
+        super().__init__(speed, buffer)
+        self.products = products
+    
+    def run(self):
+        while True:
+            self.product = choice(self.products)
+            self.simulate_work()
+            self.buffer.put(self.product)
+            self.simulate_idle()
 
 
 
