42549b145cd38d09d6544dabdfad0589e2675170
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-13 23:09:39 +0800

    added class consumer

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -88,6 +88,14 @@ class Producer(Worker):
             self.buffer.put(self.product)
             self.simulate_idle()
 
+class Consumer(Worker):
+    def run(self):
+        while True:
+            self.product = self.buffer.get()
+            self.simulate_work()
+            self.buffer.task_done()
+            self.simulate_idle()
+
 
 
 
