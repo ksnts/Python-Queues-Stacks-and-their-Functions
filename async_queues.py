1e0f0e8e727b207590cdebf67d9ade7e043e44a3
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-14 00:58:34 +0800

    updated main with more queue tasks and worker identifiers

--- async_queues.py
+++ async_queues.py
@@ -12,7 +12,30 @@ async def main(args):
     session = aiohttp.ClientSession()
     try:
         links = Counter()
+        queue = asyncio.Queue()
+        tasks = [
+            asyncio.create_task(
+                worker(
+                    f"Worker-{i + 1}",
+                    session,
+                    queue,
+                    links,
+                    args.max_depth,
+                )
+            )
+            for i in range(args.num_workers)
+        ]
+
+        await queue.put(Job(args.url))
+        await queue.join()
+
+        for task in tasks:
+            task.cancel()
+
+        await asyncio.gather(*tasks, return_exceptions=True)
+
         display(links)
+        
     finally:
         await session.close()
 
@@ -61,3 +84,5 @@ async def worker(worker_id, session, queue, links, max_depth):
             print(f"[{worker_id} failed at {url=}]", file=sys.stderr)
         finally:
             queue.task_done()
+
+
