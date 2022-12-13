bf938a9c19861a7fc9c5b76f85023c618a185869
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-14 00:36:56 +0800

    added fetching and parsing functions

--- async_queues.py
+++ async_queues.py
@@ -1,8 +1,10 @@
 import argparse
 import asyncio
 from collections import Counter
-
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 import aiohttp
+
 
 async def main(args):
     session = aiohttp.ClientSession()
@@ -25,3 +27,15 @@ def display(links):
 
 if __name__ == "__main__":
     asyncio.run(main(parse_args()))
+
+async def fetch_html(session, url):
+    async with session.get(url) as response:
+        if response.ok and response.content_type == "text/html":
+            return await response.text()
+
+def parse_links(url, html):
+    soup = BeautifulSoup(html, features="html.parser")
+    for anchor in soup.select("a[href]"):
+        href = anchor.get("href").lower()
+        if not href.startswith("javascript:"):
+            yield urljoin(url, href)
