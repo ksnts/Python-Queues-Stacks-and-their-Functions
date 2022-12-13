d3dcb3f60af44dab65476df555737880778b812c
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-13 23:42:04 +0800

    Added class view and imported rich

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -4,6 +4,13 @@ import threading
 from random import randint
 from time import sleep
 from random import choice, randint
+from itertools import zip_longest
+
+from rich.align import Align
+from rich.columns import Columns
+from rich.console import Group
+from rich.live import Live
+from rich.panel import Panel
 
 QUEUE_TYPES = {
     "fifo": Queue,
@@ -13,6 +20,22 @@ QUEUE_TYPES = {
 
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
+    producers = [
+        Producer(args.producer_speed, buffer, PRODUCTS)
+        for _ in range(args.producers)
+    ]
+    consumers = [
+        Consumer(args.consumer_speed, buffer) for _ in range(args.consumers)
+    ]
+
+    for producer in producers:
+        producer.start()
+
+    for consumer in consumers:
+        consumer.start()
+
+    view = View(buffer, producers, consumers)
+    view.animate()
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -95,6 +118,54 @@ class Consumer(Worker):
             self.simulate_work()
             self.buffer.task_done()
             self.simulate_idle()
+
+
+class View:
+    def __init__(self, buffer, producers, consumers):
+        self.buffer = buffer
+        self.producers = producers
+        self.consumers = consumers
+
+    def animate(self):
+        with Live(
+            self.render(), screen=True, refresh_per_second=10
+        ) as live:
+            while True:
+                live.update(self.render())
+
+    def render(self):
+
+        match self.buffer:
+            case PriorityQueue():
+                title = "Priority Queue"
+                products = map(str, reversed(list(self.buffer.queue)))
+            case LifoQueue():
+                title = "Stack"
+                products = list(self.buffer.queue)
+            case Queue():
+                title = "Queue"
+                products = reversed(list(self.buffer.queue))
+            case _:
+                title = products = ""
+
+        rows = [
+            Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)
+        ]
+        pairs = zip_longest(self.producers, self.consumers)
+        for i, (producer, consumer) in enumerate(pairs, 1):
+            left_panel = self.panel(producer, f"Producer {i}")
+            right_panel = self.panel(consumer, f"Consumer {i}")
+            rows.append(Columns([left_panel, right_panel], width=40))
+        return Group(*rows)
+
+    def panel(self, worker, title):
+        if worker is None:
+            return ""
+        padding = " " * int(29 / 100 * worker.progress)
+        align = Align(
+            padding + worker.state, align="left", vertical="middle"
+        )
+        return Panel(align, height=5, title=title)
 
 
 
