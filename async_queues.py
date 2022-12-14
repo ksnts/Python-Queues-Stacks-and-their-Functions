30b47208a6f5e7c0cb2fffe328071ceff54f5874
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-14 23:14:18 +0800

    Organized code placements

--- async_queues.py
+++ async_queues.py
@@ -1,12 +1,59 @@
 import argparse
 import asyncio
 from collections import Counter
+import aiohttp
+
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
-import aiohttp
+
 import sys
 from typing import NamedTuple
 
+class Job(NamedTuple):
+    url: str
+    depth: int = 1
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    parser.add_argument("-d", "--max-depth", type=int, default=2)
+    parser.add_argument("-w", "--num-workers", type=int, default=3)
+    return parser.parse_args()
+
+def display(links):
+    for url, count in links.most_common():
+        print(f"{count:>3} {url}")
+
+
+
+async def fetch_html(session, url):
+    async with session.get(url) as response:
+        if response.ok and response.content_type == "text/html":
+            return await response.text()
+
+def parse_links(url, html):
+    soup = BeautifulSoup(html, features="html.parser")
+    for anchor in soup.select("a[href]"):
+        href = anchor.get("href").lower()
+        if not href.startswith("javascript:"):
+            yield urljoin(url, href)
+
+
+async def worker(worker_id, session, queue, links, max_depth):
+    print(f"[{worker_id} starting]", file=sys.stderr)
+    while True:
+        url, depth = await queue.get()
+        links[url] += 1
+        try:
+            if depth <= max_depth:
+                print(f"[{worker_id} {depth=} {url=}]", file=sys.stderr)
+                if html := await fetch_html(session, url):
+                    for link_url in parse_links(url, html):
+                        await queue.put(Job(link_url, depth + 1))
+        except aiohttp.ClientError:
+            print(f"[{worker_id} failed at {url=}]", file=sys.stderr)
+        finally:
+            queue.task_done()
 
 async def main(args):
     session = aiohttp.ClientSession()
@@ -39,50 +86,5 @@ async def main(args):
     finally:
         await session.close()
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url")
-    parser.add_argument("-d", "--max-depth", type=int, default=2)
-    parser.add_argument("-w", "--num-workers", type=int, default=3)
-    return parser.parse_args()
-
-def display(links):
-    for url, count in links.most_common():
-        print(f"{count:>3} {url}")
-
 if __name__ == "__main__":
     asyncio.run(main(parse_args()))
-
-async def fetch_html(session, url):
-    async with session.get(url) as response:
-        if response.ok and response.content_type == "text/html":
-            return await response.text()
-
-def parse_links(url, html):
-    soup = BeautifulSoup(html, features="html.parser")
-    for anchor in soup.select("a[href]"):
-        href = anchor.get("href").lower()
-        if not href.startswith("javascript:"):
-            yield urljoin(url, href)
-
-class Job(NamedTuple):
-    url: str
-    depth: int = 1
-
-async def worker(worker_id, session, queue, links, max_depth):
-    print(f"[{worker_id} starting]", file=sys.stderr)
-    while True:
-        url, depth = await queue.get()
-        links[url] += 1
-        try:
-            if depth <= max_depth:
-                print(f"[{worker_id} {depth=} {url=}]", file=sys.stderr)
-                if html := await fetch_html(session, url):
-                    for link_url in parse_links(url, html):
-                        await queue.put(Job(link_url, depth + 1))
-        except aiohttp.ClientError:
-            print(f"[{worker_id} failed at {url=}]", file=sys.stderr)
-        finally:
-            queue.task_done()
-
-
