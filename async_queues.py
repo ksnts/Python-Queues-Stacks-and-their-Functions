254b7d4d79fed69797c02132c14bad593d80e0d2
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-14 23:27:48 +0800

    changed to priorityqueue

--- async_queues.py
+++ async_queues.py
@@ -63,7 +63,7 @@ async def main(args):
     session = aiohttp.ClientSession()
     try:
         links = Counter()
-        queue = asyncio.LifoQueue()
+        queue = asyncio.PriorityQueue()
         tasks = [
             asyncio.create_task(
                 worker(
