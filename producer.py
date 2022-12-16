bb8881af146f1f87a047b769e627f425abbb3337
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-12-16 15:13:04 +0800

    reverted file, found that changes are for another python file

--- producer.py
+++ producer.py
@@ -2,20 +2,9 @@ import pika
 
 QUEUE_NAME = "mailbox"
 
-def callback(channel, method, properties, body):
-    message = body.decode("utf-8")
-    print(f"Got message: {message}")
-
 with pika.BlockingConnection() as connection:
     channel = connection.channel()
     channel.queue_declare(queue=QUEUE_NAME)
-    channel.basic_consume(
-        queue=QUEUE_NAME,
-        auto_ack=True,
-        on_message_callback=callback
-    )
-    channel.start_consuming()
-    
     while True:
         message = input("Message: ")
         channel.basic_publish(
@@ -23,5 +12,3 @@ with pika.BlockingConnection() as connection:
             routing_key=QUEUE_NAME,
             body=message.encode("utf-8")
         )
-
-
